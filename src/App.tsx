335843c98db56a33210b308862c5bbad261ca240
ivanvinicius <ivanvinicius@github.com>
2024-02-19 20:21:21 -0300

    :sparkles: shadcn tailwind eslint prettier config

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './globals.css'
+
+export function App() {
+  return (
+    <div className="flex items-center justify-between bg-red-500 font-semibold text-white">
+      <span className="text-2xl font-bold text-white">Hello</span>
+    </div>
+  )
+}
