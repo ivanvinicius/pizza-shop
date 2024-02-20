865cf00076bd1b125eda6d1f099938c91b218ce9
ivanvinicius <ivanvinicius@github.com>
2024-02-20 14:17:02 -0300

    :sparkles: helmet async

--- src/pages/app/dashborad.tsx
+++ src/pages/app/dashborad.tsx
@@ -1,7 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function Dashborad() {
   return (
-    <div>
+    <>
+      <Helmet title="Dashboard" />
       <span>dashborad</span>
-    </div>
+    </>
   )
 }
