62fcba1fdfe1a40a519e811b28465660326aa337
ivanvinicius <ivanvinicius@github.com>
2024-02-20 14:11:13 -0300

    :sparkles: layouts

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashborad } from './pages/app/dashborad'
 import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <Dashborad /> },
-  { path: '/sign-in', element: <SignIn /> },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashborad /> }],
+  },
+
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
+  },
 ])
