876dda2f4c37e6aef4386c07c94a6d468e9c6854
ivanvinicius <ivanvinicius@github.com>
2024-02-21 10:18:28 -0300

    :sparkles: tabela e filtros

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,48 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '~/components/ui/button'
+import { TableCell, TableRow } from '~/components/ui/table'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button size="xs" variant="outline" title="Detalhes do pedido">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+
+      <TableCell className="font-mono text-sm font-medium">
+        31das235sad13254hgfa
+      </TableCell>
+
+      <TableCell className="text-muted-foreground">Há 15 minutos</TableCell>
+
+      <TableCell className="">
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+
+      <TableCell className="font-medium">Diego Fernandes</TableCell>
+
+      <TableCell className="font-medium">R$ 149,90</TableCell>
+
+      <TableCell>
+        <Button size="xs" variant="outline">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+
+      <TableCell>
+        <Button size="xs" variant="ghost">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
